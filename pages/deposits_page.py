91e1994708e301a66bfe3a49120bb34d474e11d3
Leandro Stevanato Alves <leandro.stevanato@gmail.com>
2024-03-02 23:32:28 -0300

    Change from CRLF to LF

--- pages/deposits_page.py
+++ pages/deposits_page.py
@@ -8,11 +8,6 @@ class DepositsPage:
         self.driver = driver
         self.wait = wait
         self.new_deposit_link = (By.LINK_TEXT, 'New deposit')
-        #<p style="color: green">Deposit was successfully destroyed.</p>
-        #self.deposits_menu = (By.LINK_TEXT, 'Deposits')
-        #self.items_menu = (By.LINK_TEXT, 'Items')
-        #self.signed_in_message = (By.TAG_NAME, 'p')
-        #self.welcome_message = (By.TAG_NAME, 'h1')
 
     def click_new_deposit_link(self):
         self.wait.until(ec.element_to_be_clickable(self.new_deposit_link)).click()
